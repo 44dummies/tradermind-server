cfdecbbfe9d309c561f75b02aeba4462a2656174
44dummies <muindidamian@gmail.com>
2025-12-08 12:10:37 +0300

    feat: Add admin users API routes
    - GET /admin/users - list all users
    - GET /admin/users/:userId - get user details
    - PUT /admin/users/:userId/role - update admin role
    - DELETE /admin/users/:userId - deactivate user

--- src/routes/admin/index.js
+++ src/routes/admin/index.js
@@ -8,6 +8,7 @@ const statsRoutes = require('./stats');
 const notificationRoutes = require('./notifications');
 const logsRoutes = require('./logs');
 const recoveryRoutes = require('./recovery');
+const usersRoutes = require('./users');
 
 // Mount routes
 router.use('/bot', botRoutes);
@@ -16,5 +17,7 @@ router.use('/stats', statsRoutes);
 router.use('/notifications', notificationRoutes);
 router.use('/logs', logsRoutes);
 router.use('/recovery', recoveryRoutes);
+router.use('/users', usersRoutes);
 
 module.exports = router;
+
